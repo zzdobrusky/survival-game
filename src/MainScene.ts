bdaf66bc7be36948a8c196f242f8929f4d57cd2f
Zbynek Dusatko <zzdobrusky@gmail.com>
2021-12-19 22:19:48 -0700

    added map collider

--- src/MainScene.ts
+++ src/MainScene.ts
@@ -19,6 +19,8 @@ export default class MainScene extends Phaser.Scene {
     const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
     const layer1 = map.createLayer('Tile Layer 1', tileset, 0, 0);
     const layer2 = map.createLayer('Tile Layer 2', tileset, 0, 0);
+    layer1.setCollisionByProperty({ collides: true });
+    this.matter.world.convertTilemapLayer(layer1);
     this.player = new Player({
       scene: this,
       x: 100,
