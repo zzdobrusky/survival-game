0db6fb2bbd29b0f5f791daa5df72a1c7e15ac919
Zbynek Dusatko <zzdobrusky@gmail.com>
2022-01-16 18:17:56 -0700

    added a pickaxe

--- src/Player.ts
+++ src/Player.ts
@@ -1,38 +1,56 @@
-type WSADKeys = {
-  up: Phaser.Input.Keyboard.Key;
-  down: Phaser.Input.Keyboard.Key;
-  left: Phaser.Input.Keyboard.Key;
-  right: Phaser.Input.Keyboard.Key;
+type KeyboardKeys = {
+  upW: Phaser.Input.Keyboard.Key;
+  downS: Phaser.Input.Keyboard.Key;
+  leftA: Phaser.Input.Keyboard.Key;
+  rightArrow: Phaser.Input.Keyboard.Key;
+  upArrow: Phaser.Input.Keyboard.Key;
+  downArrow: Phaser.Input.Keyboard.Key;
+  leftArrow: Phaser.Input.Keyboard.Key;
+  rightD: Phaser.Input.Keyboard.Key;
+  EKey: Phaser.Input.Keyboard.Key;
+  pointer: Phaser.Input.Pointer;
 };
 
 export default class Player extends Phaser.Physics.Matter.Sprite {
-  private wsadKeys: WSADKeys;
+  private keys: KeyboardKeys;
+  private spriteWeapon: Phaser.GameObjects.Sprite;
+  private weaponRotation: number;
+  private weaponRotationDirection: number;
 
   constructor(data) {
     const { scene, x, y, texture, frame } = data;
     super(scene.matter.world, x, y, texture, frame);
+    scene.add.existing(this);
+    // Weapen
+    this.spriteWeapon = new Phaser.GameObjects.Sprite(this.scene, 0, 0, 'items', 162);
+    this.spriteWeapon.setScale(0.8);
+    this.spriteWeapon.setOrigin(0.25, 0.75);
+    this.weaponRotationDirection = 1;
+    scene.add.existing(this.spriteWeapon);
+
     // added WSAD keys
-    this.wsadKeys = {
-      up: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
-      down: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
-      left: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
-      right: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+    this.keys = {
+      upW: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+      downS: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+      leftA: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+      rightD: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+      rightArrow: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT),
+      upArrow: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP),
+      downArrow: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN),
+      leftArrow: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT),
+      EKey: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
+      pointer: scene.input.activePointer,
     };
 
-    // this.scaleX *= 2;
-    // this.scaleY *= 2;
-
     this.setCircle(12);
-
     this.setFrictionAir(0.35);
     this.setFixedRotation();
-
-    scene.add.existing(this);
   }
 
   public static preload(scene: Phaser.Scene): void {
     scene.load.atlas('female', 'assets/animations/female.png', 'assets/animations/female_atlas.json');
     scene.load.animation('female_anim', 'assets/animations/female_anim.json');
+    scene.load.spritesheet('items', 'assets/images/items.png', { frameWidth: 32, frameHeight: 32 });
   }
 
   get velocity(): MatterJS.Vector {
@@ -42,15 +60,17 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
   public update(): void {
     const speed = 2.5;
     const playerVelocity = new Phaser.Math.Vector2();
-    if (this.wsadKeys.left.isDown) {
+
+    // move player around
+    if (this.keys.leftA.isDown || this.keys.leftArrow.isDown) {
       playerVelocity.x = -1;
-    } else if (this.wsadKeys.right.isDown) {
+    } else if (this.keys.rightD.isDown || this.keys.rightArrow.isDown) {
       playerVelocity.x = 1;
     }
 
-    if (this.wsadKeys.up.isDown) {
+    if (this.keys.upW.isDown || this.keys.upArrow.isDown) {
       playerVelocity.y = -1;
-    } else if (this.wsadKeys.down.isDown) {
+    } else if (this.keys.downS.isDown || this.keys.downArrow.isDown) {
       playerVelocity.y = 1;
     }
 
@@ -67,9 +87,25 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         (Math.sign(this.velocity.x) === 1 && playerDirection === -1)
       ) {
         this.scaleX *= -1;
+        this.spriteWeapon.scaleX *= -1;
+        this.weaponRotationDirection *= -1;
       }
     } else {
       this.anims.play('female_idle', true);
     }
+
+    this.spriteWeapon.setPosition(this.x, this.y);
+    // weapon rotate
+    if (this.keys.pointer.isDown || this.keys.EKey.isDown) {
+      console.log('weaponRotationDirection: ', this.weaponRotationDirection);
+      this.weaponRotation += this.weaponRotationDirection * 6;
+    } else {
+      this.weaponRotation = 0;
+    }
+    console.log(this.weaponRotation);
+    if (this.weaponRotation > 100 || this.weaponRotation < -100) {
+      this.weaponRotation = 0;
+    }
+    this.spriteWeapon.setAngle(this.weaponRotation);
   }
 }
