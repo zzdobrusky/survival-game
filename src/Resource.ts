356a54f759b7e4defc02ba9528bd6f55146fe43c
Zbynek Dusatko <zzdobrusky@gmail.com>
2022-01-22 22:21:18 -0700

    added sound FXs

--- src/Resource.ts
+++ src/Resource.ts
@@ -3,10 +3,10 @@ import MainScene from './MainScene';
 const CIRCLE_RADIUS = 12;
 
 export default class Resource extends Phaser.Physics.Matter.Sprite {
-  public health: number;
-
+  private _health: number;
   private _mainScene: MainScene;
   private _circle: Phaser.Geom.Circle;
+  private _sound;
 
   constructor({ scene, resource }: { scene: MainScene; resource: any }) {
     super(scene.matter.world, resource.x, resource.y, 'resources', resource.type);
@@ -20,15 +20,20 @@ export default class Resource extends Phaser.Physics.Matter.Sprite {
     this.setStatic(true);
     this.setOrigin(0.5, yOrigin);
     this.type = resource.type;
-    this.health = 5;
+    this._health = 5;
+    // add specific sound by the resource type
+    this._sound = this._mainScene.sound.add(this.type, { loop: false });
   }
 
   public static preload(scene: Phaser.Scene): void {
     scene.load.atlas('resources', 'assets/images/resources.png', 'assets/images/resources_atlas.json');
+    scene.load.audio('tree', 'assets/audio/tree.mp3');
+    scene.load.audio('rock', 'assets/audio/rock.wav');
+    scene.load.audio('bush', 'assets/audio/bush.wav');
   }
 
   get dead(): boolean {
-    return this.health <= 0;
+    return this._health <= 0;
   }
 
   get circle(): Phaser.Geom.Circle {
@@ -37,7 +42,8 @@ export default class Resource extends Phaser.Physics.Matter.Sprite {
 
   public hit(): void {
     // if (this.sound) this.sound.play(); // TODO:
-    this.health--;
-    console.log(`Hitting: ${this.type} Health: ${this.health}`);
+    this._health--;
+    this._sound.play();
+    console.log(`Hitting: ${this.type} Health: ${this._health}`);
   }
 }
